c69146c6b118752391ed236f2e1f13a69862904b
Anthooooony <anthooooony@icloud.com>
2026-04-16 11:55:47 +0800

    添加 StreamFilter 接口定义及 FindStream 查找方法

--- filter/filter.go
+++ filter/filter.go
@@ -26,3 +26,24 @@ type Filter interface {
 	// ApplyOnError 在命令执行失败时(exit!=0)应用过滤，返回 nil 表示直接透传原始输出
 	ApplyOnError(input FilterInput) *FilterOutput
 }
+
+// StreamAction 表示流式过滤中对单行的决策
+type StreamAction int
+
+const (
+	StreamDrop StreamAction = iota // 丢弃此行
+	StreamEmit                     // 立即输出此行
+)
+
+// StreamFilter 是支持流式（逐行）过滤的接口。
+// 有状态设计：每次命令执行调用 NewStreamInstance() 创建新处理器实例。
+type StreamFilter interface {
+	Filter
+	NewStreamInstance() StreamProcessor
+}
+
+// StreamProcessor 是单次命令执行的流式处理器，持有本次执行的状态。
+type StreamProcessor interface {
+	ProcessLine(line string) (action StreamAction, output string)
+	Flush(exitCode int) []string
+}
